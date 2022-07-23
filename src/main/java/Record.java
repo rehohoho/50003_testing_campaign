a4d767f483f5484ae1756f8e4672ef06e1c32af1
rehohoho <ernie.birdy.ho@gmail.com>
2022-07-23 17:22:41 +0800

    CE10: Check formatting of file before processing.

--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
 
 /**
@@ -18,6 +20,13 @@ public class Record {
 
     private void readRecords(String path) throws FileNotFoundException, IOException {
         BufferedReader br = new BufferedReader(new FileReader(path));
+        
+        String formatting = Files.probeContentType(Paths.get(path));
+        if (formatting == null || !formatting.equals("application/vnd.ms-excel")) {
+            br.close();
+            throw new IOException("unexpected file formatting: " + formatting);
+        }
+
         String line;
         while ((line = br.readLine()) != null) {
             line = line.replace("\"", "");
