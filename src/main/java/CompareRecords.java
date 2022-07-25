fc221a4d058e7a3abaffb5d22b7e8d52fa370e8e
rehohoho <ernie.birdy.ho@gmail.com>
2022-07-25 18:22:59 +0800

    CE10: refactor part of main, update gitignore

--- src/main/java/CompareRecords.java
+++ src/main/java/CompareRecords.java
@@ -12,16 +12,14 @@ public class CompareRecords {
     {
         Record record1 = new Record(args[0]);
         Record record2 = new Record(args[1]);
+        Object[] res = compare(record1, record2);
         
-        HashSet<String> res = new HashSet<String>(record1.getEntries());
-        res.removeAll(record2.getEntries());
-        record2.getEntries().removeAll(record1.getEntries());
-        res.addAll(record2.getEntries());
-        
-        Object[] resArr = res.toArray();
-        Arrays.sort(resArr);
-        writeResult(resArr, args[2]);
-        System.out.println("Result csv outputted at " + args[2]);
+        if (args[2] != null) {
+            writeResult(res, args[2]);
+            System.out.println("Result csv outputted at " + args[2]);
+        } else {
+            System.out.println(res);
+        }
     }
 
     public static void writeResult(Object[] res, String path) {
@@ -35,6 +33,17 @@ public class CompareRecords {
         } catch (IOException e) {
             System.err.println(e);
         }
+    }
+
+    private static Object[] compare(Record record1, Record record2) {
+        HashSet<String> res = new HashSet<String>(record1.getEntries());
+        res.removeAll(record2.getEntries());
+        record2.getEntries().removeAll(record1.getEntries());
+        res.addAll(record2.getEntries());
+        
+        Object[] resArr = res.toArray();
+        Arrays.sort(resArr);
+        return resArr;
     }
 
 }
